d7488cbb4510c5c7c3596f01e72a5acd2f805434
Kevin Ly <kly1@avans.nl>
2018-05-16 23:56:56 +0200

    Added migrations and fixed Dependency Injections

--- ContainerService.Infrastructure/DI/DIHelper.cs
+++ ContainerService.Infrastructure/DI/DIHelper.cs
@@ -3,6 +3,10 @@ using Microsoft.Extensions.Configuration;
 using System;
 using ContainerService.Core.Messaging;
 using ContainerService.Infrastructure.Messaging;
+using ContainerService.Infrastructure.Database;
+using ContainerService.Core.Repositories;
+using ContainerService.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContainerService.Infrastructure.DI
 {
@@ -10,7 +14,11 @@ namespace ContainerService.Infrastructure.DI
 	{
 		public static void Setup(IServiceCollection services, IConfiguration configuration)
 		{
+			services.AddDbContext<ContainerDbContext>(opt => opt.UseSqlServer(configuration.GetSection("DB_CONNECTION_STRING").Value));
 
+			services.AddTransient<IContainerRepository, ContainerRepository>();
+			services.AddTransient<IShipRepository, ShipRepository>();
+			services.AddTransient<ITruckRepository, TruckRepository>();
 
 			services.AddSingleton<IMessageHandler, RabbitMQMessageHandler>((provider) => new RabbitMQMessageHandler(configuration.GetSection("AMQP_URL").Value));
 			services.AddTransient<IMessagePublisher, RabbitMQMessagePublisher>((provider) => new RabbitMQMessagePublisher(configuration.GetSection("AMQP_URL").Value));
@@ -18,7 +26,10 @@ namespace ContainerService.Infrastructure.DI
 
 		public static void OnServicesSetup(IServiceProvider serviceProvider)
 		{
-
+			Console.WriteLine("Connecting to database and migrating if required");
+			var dbContext = serviceProvider.GetService<ContainerDbContext>();
+			dbContext.Database.Migrate();
+			Console.WriteLine("Completed connecting to database");
 		}
 	}
 }
---
+++ ContainerService.Infrastructure/Migrations/20180516215641_Initial.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ContainerService.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShipService",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShipService", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trucks",
+                columns: table => new
+                {
+                    LicensePlate = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trucks", x => x.LicensePlate);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    ShipServiceId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ships_ShipService_ShipServiceId",
+                        column: x => x.ShipServiceId,
+                        principalTable: "ShipService",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ProductType = table.Column<int>(nullable: false),
+                    SerialShippingContainerCode = table.Column<string>(nullable: true),
+                    ShipId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Containers_Ships_ShipId",
+                        column: x => x.ShipId,
+                        principalTable: "Ships",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ContainerId = table.Column<Guid>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ProductType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Containers_ContainerId",
+                        column: x => x.ContainerId,
+                        principalTable: "Containers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Containers_ShipId",
+                table: "Containers",
+                column: "ShipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ContainerId",
+                table: "Product",
+                column: "ContainerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ships_ShipServiceId",
+                table: "Ships",
+                column: "ShipServiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Trucks");
+
+            migrationBuilder.DropTable(
+                name: "Containers");
+
+            migrationBuilder.DropTable(
+                name: "Ships");
+
+            migrationBuilder.DropTable(
+                name: "ShipService");
+        }
+    }
+}
